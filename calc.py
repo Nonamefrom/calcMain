c49ba35d990d90e5f4919c54533dbcff55cbae89
Nonamefrom <79928776+Nonamefrom@users.noreply.github.com>
2021-03-13 16:50:32 +0300

    код calc

---
+++ calc.py
@@ -0,0 +1,18 @@
+#calculyator
+def calc_plus(num1,num2):
+    result=num1+num2
+    print(round(result,2))
+calc_plus(2,5.124)
+def calc_minus(num1,num2):
+    result=num1-num2
+    print(round(result,2))
+calc_minus(2,5.124)
+def calc_division(num1,num2):
+    result=num1/num2
+    print(round(result,2))
+calc_division(2,5.124)
+def calc_multi(num1,num2):
+    result=num1*num2
+    print(round(result,2))
+calc_multi(2,5.124)
+git init
