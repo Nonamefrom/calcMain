d49f1f1fb75650395ea3449af786ca488c8e71ac
Nonamefrom <Kabayashemaru@yandex.ru>
2021-03-16 20:31:53 +0300

    new code calc

--- calc.py
+++ calc.py
@@ -1,17 +1,18 @@
-#calculyator
-def calc_plus(num1,num2):
-    result=num1+num2
-    print(round(result,2))
-calc_plus(2,5.124)
-def calc_minus(num1,num2):
-    result=num1-num2
-    print(round(result,2))
-calc_minus(2,5.124)
-def calc_division(num1,num2):
-    result=num1/num2
-    print(round(result,2))
-calc_division(2,5.124)
-def calc_multi(num1,num2):
-    result=num1*num2
-    print(round(result,2))
-calc_multi(2,5.124)
+#
+user_input = input('Введите через запятую и пробел: число1, знак(+,-,*,/), число2 ')
+str_input = user_input.split(', ')
+num1 = int(str_input[0])
+sign = str_input[1]
+num2 = int(str_input[2])
+#строка ниже попытка вывести строку со знаком - не заработало
+#print (f'Ответ: {num1} {sign} {num2} = {num1signnum2}')
+if sign == '+':
+    print (f'Ответ: {num1} {sign} {num2} = {num1 + num2}')
+elif sign == '-':
+    print (f'Ответ: {num1} {sign} {num2} = {num1 - num2}')
+elif sign == '*':
+    print (f'Ответ: {num1} {sign} {num2} = {num1 * num2}')
+elif sign == '/':
+    print (f'Ответ: {num1} {sign} {num2} = {num1 / num2}')
+else:
+    print('Несоблюдены условия ввода данных!')
